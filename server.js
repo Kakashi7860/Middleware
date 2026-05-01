7577937e5421faab8bf55313723eb892e630fd91
Kakashi7860 <sohelshakh91@gmail.com>
2026-05-01 18:50:40 +0530

    first commit

--- server.js
+++ server.js
@@ -3,35 +3,43 @@ const app = express()
 
 app.use(express.json())
 
-let users =[]
+let users = []
 let idcounter = 1
 
-app.post("/addUser",(req,res)=>{
-    let {name,age} = req.body;
+app.post("/addUser", (req, res) => {
+    let { name, age, contact } = req.body;
 
-    if(!name || name.length < 3){
-        return res.status(400).send("name must be atlest 3 char");
-    }
-    if(!age || age >= 18){
-        return res.status(400).send("must be 18+");
 
+    if (!name || name.length < 3) {
+        return res.status(400).send("Name must be at least 3 characters");
     }
 
+    if (!age || age < 18) {
+        return res.status(400).send("User must be 18+");
+    }
 
 
+    if (!contact || contact.length !== 10) {
+        return res.status(400).send("Contact must be 10 digits");
+    }
 
-
+    for (let i = 0; i < contact.length; i++) {
+        if (contact[i] < '0' || contact[i] > '9') {
+            return res.status(400).send("Contact must contain only numbers");
+        }
+    }
 
     let user = {
         id: idcounter++,
-        name: req.body.name,
-        age: req.body.age
+        name,
+        age,
+        contact
     }
+
     users.push(user)
     res.json(user)
 })
 
-
-app.listen(3000,()=>{
-    console.log("server is running")
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
 })
