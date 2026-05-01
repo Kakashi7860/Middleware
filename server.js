690413ce2c9b701163462d9d519650ccbda10e58
Kakashi7860 <sohelshakh91@gmail.com>
2026-05-01 18:10:34 +0530

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require("express")
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log("method",req.method)
-    console.log("url",req.url)
-    next()
+app.use(express.json())
+
+let users =[]
+let idcounter = 1
+
+app.post("/addUser",(req,res)=>{
+    let {name,age} = req.body;
+
+    if(!name || name.length < 3){
+        return res.status(400).send("name must be atlest 3 char");
+    }
+    if(!age || age >= 18){
+        return res.status(400).send("must be 18+");
+
+    }
+
+
+
+
+
+
+    let user = {
+        id: idcounter++,
+        name: req.body.name,
+        age: req.body.age
+    }
+    users.push(user)
+    res.json(user)
 })
-
-
-app.get("/",(req,res)=>{
-    res.send("home page")
-
-});
 
 
 app.listen(3000,()=>{
